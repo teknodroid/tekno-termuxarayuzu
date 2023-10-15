e8e906d2f6ed5508c0235d0bf5008e523522eee1
TEKNODROİD <123983520+teknodroid@users.noreply.github.com>
2023-10-15 13:01:19 +0300

    Update .banner.sh

--- .banner.sh
+++ .banner.sh
@@ -1,94 +1,38 @@
-[[ $- != *i* ]] && return
+#!/usr/bin/bash
+var=$(echo $(( ${1} - 1)))
+var2=$(seq -s─ ${var}|tr -d '[:digit:]')
+var3=$(seq -s\  ${var}|tr -d '[:digit:]')
+var4=$(echo $(( ${1} - 20)))
+rm -rf ~/.draw.sh
+cat >> ~/.draw.sh << EOF
+#!/usr/bin/bash
+PUT(){ echo -en "\033[\${1};\${2}H";}
+DRAW(){ echo -en "\033%";echo -en "\033(0";}
+WRITE(){ echo -en "\033(B";}
+HIDECURSOR(){ echo -en "\033[?25l";}
+NORM(){ echo -en "\033[?12l\033[?25h";}
 
-shopt -s checkwinsize
-shopt -s cmdhist
-shopt -s globstar
-shopt -s histappend
-shopt -s histverify
-
-## Configure bash history.
-if [ "$(id -u)" = "0" ]; then
-	export HISTFILE="$HOME/.bash_root_history"
-else
-	export HISTFILE="$HOME/.bash_history"
-fi
-export HISTSIZE=4096
-export HISTFILESIZE=16384
-export HISTCONTROL="ignoreboth"
-
-## cursor
-printf '\e[4 q'
-
-## loader
-. ~/.draw;
-
-## Prompt.
-. ~/.username
-PROMPT_DIRTRIM=2
-if [ "$(id -u)" = "0" ]; then
-	PS1="\\[\\e[0;31m\\]\\w\\[\\e[0m\\] \\[\\e[0;97m\\]\\$\\[\\e[0m\\] "
-else
-	PS1="
-\[\033[0;31m\]┌─[\[\033[1;34m\]$NAME\[\033[1;33m\]@\[\033[1;36m\]termux\[\033[0;31m\]]─[\[\033[0;32m\]\w\[\033[0;31m\]]
-\[\033[0;31m\]└──╼ \[\e[1;31m\]❯\[\e[1;34m\]❯\[\e[1;90m\]❯\[\033[0m\] "
-fi
-PS2='> '
-PS3='> '
-PS4='+ '
-## Terminal title.
-case "$TERM" in
-	xterm*|rxvt*)
-		if [ "$(id -u)" = "0" ]; then
-			PS1="\\[\\e]0;termux (root): \\w\\a\\]$PS1"
-		else
-			PS1="\\[\\e]0;termux: \\w\\a\\]$PS1"
-		fi
-		;;
-	*)
-		;;
-esac
-
-## Colorful output & useful aliases for 'ls' and 'grep'.
-if [ -x "$PREFIX/bin/dircolors" ] && [ -n "$LOCAL_PREFIX" ]; then
-	if [ -f "$LOCAL_PREFIX/etc/dircolors.conf" ]; then
-		eval "$(dircolors -b "$LOCAL_PREFIX/etc/dircolors.conf")"
-	fi
-fi
-
-## Colored output.
-alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias dir='dir --color=auto'
-alias vdir='vdir --color=auto -h'
-
-## Replace 'cat' with 'bat' (if available).
-if [ -n "$(command -v bat)" ]; then
-	alias cat='bat --color=never --decorations=never --paging=never'
-fi
-
-## Replace 'ls' with 'exa' (if available) + some aliases.
-if [ -n "$(command -v exa)" ]; then
-	alias l='exa'
-	alias ls='exa'
-	alias l.='exa -d .*'
-	alias la='exa -a'
-	alias ll='exa -Fhl'
-	alias ll.='exa -Fhl -d .*'
-else
-	alias l='ls --color=auto'
-	alias ls='ls --color=auto'
-	alias l.='ls --color=auto -d .*'
-	alias la='ls --color=auto -a'
-	alias ll='ls --color=auto -Fhl'
-	alias ll.='ls --color=auto -Fhl -d .*'
-fi
-
-## Safety.
-alias cp='cp -i'
-alias ln='ln -i'
-alias mv='mv -i'
-alias rm='rm -i'
-
-## Proot env
-alias nethunter='proot-distro login nethunter'
+HIDECURSOR
+clear
+echo -e "\033[35;1m"
+#tput setaf 5
+echo "┌${var2}┐"
+for ((i=1; i<=8; i++)); do
+echo "│${var3}│"
+done
+echo "└${var2}┘"
+PUT 4 0
+figlet -c -f ASCII-Shadow -w ${1} "${2}" | lolcat -t
+PUT 3 0
+echo -e "\033[35;1m"
+#tput setaf 5
+for ((i=1; i<=7; i++)); do
+echo "│"
+done
+PUT 10 ${var4}
+echo -e "\e[32mBoot Script \e[33m2.0\e[0m"
+PUT 12 0
+echo
+NORM
+EOF
+bash ~/.draw.sh
