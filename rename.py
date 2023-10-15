5c29f025ad38886d66a7a5707924cbe3d4ee4e62
TEKNODROİD <123983520+teknodroid@users.noreply.github.com>
2023-10-15 13:09:01 +0300

    Create rename.py

---
+++ rename.py
@@ -0,0 +1,16 @@
+newName = input("[+] Yeni Adı Girin : ")
+newName = '"'+newName+'"'
+file  = open('/data/data/com.termux/files/home/.zshrc','r')
+allLines = []
+i = 0;pos= 0
+for line in file.readlines():
+    if "TNAME=" in line:
+        pos = i
+    allLines.append(line)
+    i+=1
+allLines[pos]="TNAME="+newName+"\n"
+file.close()
+file = open('/data/data/com.termux/files/home/.zshrc','w')
+file.writelines(allLines)
+file.close()
+print("Başarıyla güncellendi, değişiklikleri görmek için yeniden başlatın")
